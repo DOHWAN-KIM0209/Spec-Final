06e814c2a7e8163cb3044a10e6ac8cba052d460f
DOHWAN-KIM0209 <yeongtae3508@gmail.com>
2025-05-21 09:47:46 +0900

    So Many Changes

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,11 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: '@', replacement: '/src' }],
-  },
-  base: './',
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    },
+    base: './',
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:80',
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, '/api'),
+        },
+        '/ai': {
+          target: 'http://localhost:80',
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/ai/, '/ai'),
+        },
+      },
+    },
+  };
 });
